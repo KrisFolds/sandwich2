5ae9e0ea11dfe0a0bfdab1b86a29a0722f9acba4
Kris Folds <wvkidd1979@gmail.com>
2018-04-15 18:22:59 -0400

    rainy

--- app.rb
+++ app.rb
@@ -8,12 +8,24 @@ get '/get_sandwich' do
 	erb :sandwich
 end
 
-post '/sandwich_choice' do 
+post '/get_sandwich' do 
 	sandwich = params[:sandwich]
-	redirect '/final?sandwich=' + sandwich
+	redirect '/get_side?sandwich=' + sandwich
+end
+
+get '/get_side' do 
+	sandwich = params[:sandwich]
+	erb :side, :locals => {:sandwich => sandwich}
+end
+
+post '/side' do
+	sandwich = params[:sandwich]
+	side = params[:side]
+	redirect '/final?sandwich=' + sandwich + '&side=' + side
 end
 
 get '/final' do 
 	sandwich = params[:sandwich]
-	erb :final, :locals => {:sandwich => sandwich}
+	side = params[:side]
+	erb :final, :locals => {:sandwich => sandwich, :side => side}
 end	
